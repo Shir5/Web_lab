59405bc2e8afcba624bd4461b34937067580131a
Shir5 <61351477+Shir5@users.noreply.github.com>
2022-11-12 19:18:27 +0300

    Add files via upload

--- web/script.js
+++ web/script.js
@@ -1,68 +1,49 @@
-const x_buttons = document.querySelectorAll(".x");
-const x_text = document.querySelector(".form-text x-text");
-const y_button = document.querySelector(".y");
-const y_text = document.querySelector(".y-value-row");
-const r_buttons = document.querySelectorAll(".R-checkbox");
-const r_text = document.querySelector(".form-text r-text");
-const table = document.querySelector("#values");
-const submit_button = document.querySelector("#submit-button");
-const clear_button = document.querySelector(".clear-button");
-var close = document.getElementById("close"); 
-var modal = document.getElementById("modal-row");
+var form = document.querySelector('.js-form'),
+formInputs = document.querySelectorAll('.js-input'),
+inputX = document.querySelector('.regular-placeholder'),
+inputCheckbox = document.querySelector('.js-input-checkbox');
 
 
-const stats = {
-        x: undefined,
-        y: undefined,
-        r: undefined
-    };
-
-
-let buttons = [];
-
-
-
-
-function rChecker() {
-        const rCoordinatesArray = document.querySelectorAll('input[name="r"]:checked');
-        if (rCoordinatesArray.length == 1) {
-                return setSuccessFor(rCoordinatesArray[0]);
-        } else {
-                return setErrorFor(document.querySelectorAll('input[name="r"]')[0], "Выберите одно значение R");
-        }
-    }
-
-
-
-function xChecker(){
-        const x_element = document.querySelector(".x");
-        const xValue = x_element.value.replace(',','.');
-        if(xValue.includes(".")){
-            if(xValue.split('.').length > 2){
-                return setErrorFor(x_element, "Данные введены неверно");
-                
-            }
-            if (xValue.split(".")[1].length > 7){
-                return setErrorFor(x_element, "Введите число с разрядностью после запятой меньше 7");
-            }
-        }
-        if (!isNumber(xValue) || xValue === "") {
-                return setErrorFor(x_element, "X не введено");
-            } else if (parseFloat(xValue) < -3 || parseFloat(xValue) > 3) {
-                return setErrorFor(x_element, "Введите значение от -3 до 3");
-            } else {
-                return setSuccessFor(x_element);
-                
-            }
-            
+function validateX(x) {
+let re = /1/;
+return re.test(String(x).trim());
 }
 
 
-function validateForm() {
-        return xChecker()  && rChecker();
-       
+
+form.onsubmit = function() {
+let xVal = inputX.value,
+    emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+formInputs.forEach(function (input) {
+    if (input.value === '') {
+        input.classList.add('error');
+
+    } else {
+        input.classList.remove('error');
     }
-        
+});
+
+if (emptyInputs.length !== 0) {
+    console.log('inputs not filled');
+    return false;
+}
+
+if(!validateX(xVal)) {
+    console.log('X not valid');
+    inputX.classList.add('error');
+    return false;
+} else {
+    inputX.classList.remove('error');
+}
 
 
+if(!inputCheckbox.checked) {
+    console.log('checkbox not checked');
+    inputCheckbox.classList.add('error');
+    return false;
+} else {
+    inputCheckbox.classList.remove('error')
+}
 
+
+}
