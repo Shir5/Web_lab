62cffe93ede960216856830c5910cfcf667c7646
Shir5 <61351477+Shir5@users.noreply.github.com>
2022-11-15 22:02:51 +0300

    Add files via upload

--- web/script.js
+++ web/script.js
@@ -1,49 +1,99 @@
-var form = document.querySelector('.js-form'),
-formInputs = document.querySelectorAll('.js-input'),
-inputX = document.querySelector('.regular-placeholder'),
-inputCheckbox = document.querySelector('.js-input-checkbox');
+const x_buttons = document.querySelectorAll(".x");
+const x_text = document.querySelector(".x-text");
+const r_buttons = document.querySelectorAll(".R-checkbox");
+const r_text = document.querySelector(".r-text");
+const table = document.querySelector("#values");
+const submit_button = document.querySelector("#submit-button");
+const clear_button = document.querySelector(".clear-button");
+const error_text = document.querySelector("#text-error"); 
+const tool = document.querySelector(".err");
+
+const stats = {
+    x: undefined,
+    y: undefined,
+    r: undefined
+};
+
+const SELECTED_COLOR = '#1a73a8';
+const BLUR_COLOR = '#000000';
+const TEXT_COLOR = 'white';
+const HOVER_COLOR = '#2196f3';
+
+let buttons = [];
 
 
-function validateX(x) {
-let re = /^[0-3\s]*$/;
-return re.test(String(x).trim());
-}
-
-
-
-form.onsubmit = function () {
-let xVal = inputX.value,
-    emptyInputs = Array.from(formInputs).filter(input => input.value === '');
-formInputs.forEach(function (input) {
-    if (input.value === '') {
-        input.classList.add('error');
-
+function checkR(){
+    const rCoordinatesArray = document.querySelectorAll('input[name="R-checkbox"]:checked');
+    if (rCoordinatesArray.length === 1) {
+        return setSuccessFor(rCoordinatesArray[0]);
     } else {
-        input.classList.remove('error');
+        return setErrorFor(document.querySelectorAll('input[name="R-checkbox"]')[0], "Выберите одно значение R");
     }
-});
+}
 
-if (emptyInputs.length !== 0) {
-    console.log('inputs not filled');
+function isNumber(s){
+    var n = parseFloat(s.replace(',','.'));
+    return !isNaN(n) && isFinite(n);
+}
+
+function checkX(){
+    const x_element = document.querySelector(".y");
+    const xVal = x_element.value.replace(',','.');
+    if(xVal.includes(".")){
+        if(xVal.split('.').length > 2){
+            console.log('hello');
+            return setErrorFor(x_element, "Данные введены неверно");
+        }
+        if (xVal.split(".")[1].length > 7){
+            return setErrorFor(x_element, "Введите число с разрядностью после запятой меньше 7");
+        }
+    }
+    
+
+    if (!isNumber(xVal) || xVal === "") {
+        return setErrorFor(x_element, "Пожалуйста, введите значение Y");
+    } else if (parseFloat(xVal) < -5 || parseFloat(xVal) > 5) {
+        return setErrorFor(x_element, "Введите значение от -5 до 5");
+    } else {
+        return setSuccessFor(x_element);
+    }
+}
+
+
+
+function validateForm() {
+    return checkX()  && checkR();
+}
+
+function setSuccessFor(input) {
+    const err = document.querySelector('.err');
+
+    err.innerText = "";
+
+
+    return true;
+}
+
+function setErrorFor(input, message) {
+    const tool = document.querySelector('.err');
+    tool.style.color = "red"; 
+    tool.innerText = message;
     return false;
 }
 
-if(!validateX(xVal)) {
-    console.log('X not valid');
-    inputX.classList.add('error');
-    return false;
-} else {
-    inputX.classList.remove('error');
+function blurButton(button, val) {
+    if (val != button.value) {
+        button.style.backgroundColor = BLUR_COLOR;
+    } else {
+        button.style.backgroundColor = BLUR_COLOR;
+    }
 }
 
-
-if(!inputCheckbox.checked) {
-    console.log('checkbox not checked');
-    inputCheckbox.classList.add('error');
-    return false;
-} else {
-    inputCheckbox.classList.remove('error')
-}
-
-
+function uncheck(button, val) {
+    button.checked = false;
+    if (val != button.value) {
+        button.checked = false;
+    } else {
+        button.checked = true;
+    }
 }
